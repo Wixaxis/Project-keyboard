85904887b1f88e12949468ec9d01fb99a12e1cde
Wixaxis <wixaxis@gmail.com>
2021-01-04 04:17:24 +0100

    first empty :<

---
+++ KeyboardFX/src/app/App.java
@@ -0,0 +1,28 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Mainframe.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle("KeyboardFX");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ KeyboardFX/src/app/Mainframe.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class Mainframe {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
